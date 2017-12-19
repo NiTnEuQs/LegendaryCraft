50641ec77ec262dba402b2fba5262e1898f498a4
NesDev <ludovic.goldak@gmail.com>
2017-12-19 10:54:15 +0100

    Add Inventory switch

--- src/main/java/com/legendary/Application.java
+++ src/main/java/com/legendary/Application.java
@@ -54,7 +54,8 @@ public class Application implements CommandLineRunner {
         ObjectItem item4 = new ObjectItem("Anneau du bouftou", "Superbe anneau du bouftou",character1.getId(), Position.ANNEAU1);
         ObjectItem item5 = new ObjectItem("Botte du bouftou", "Superbe botte du bouftout",character1.getId(), Position.BOTTES);
         ObjectItem item6 = new ObjectItem("Ceinture du bouftout", "Permet de cultiver le blé et faire quelque dégats",character1.getId(), Position.CEINTURE);
-        ObjectItem item7 = new ObjectItem("Ceinture du tofu", "Permet de cultiver le blé et faire quelque dégats",character1.getId(), Position.INVENTORY);
+        ObjectItem item7 = new ObjectItem("Ceinture du tofu", "Ceinture du tofu hyper puissante",character1.getId(), Position.INVENTORY);
+        ObjectItem item8 = new ObjectItem("Coif du tofu", "Coif du tofu hyper puissante",character1.getId(), Position.INVENTORY);
 
 
         //objectItemRepository.save(item1);
@@ -65,6 +66,7 @@ public class Application implements CommandLineRunner {
         character1.getInventory().getObjectsItem().add(item5);
         character1.getInventory().getObjectsItem().add(item6);
         character1.getInventory().getObjectsItem().add(item7);
+        character1.getInventory().getObjectsItem().add(item8);
 
         character1.getStats().setInitialIntelligence(100);
         character1.getStats().setInitialLife(1000);
