0f36c853ac390b1e13042dd144b2763f26b66f19
nitneuqs <nitneuqtuod@hotmail.fr>
2017-12-19 10:57:35 +0100

    Version stable

--- src/main/java/com/legendary/character/inventory/ObjectItem.java
+++ src/main/java/com/legendary/character/inventory/ObjectItem.java
@@ -17,7 +17,7 @@ public class ObjectItem {
     private String description;
     private String ownerId;
     private Position position;
-    private ArrayList<ObjectEffect> effects = new ArrayList();
+    private ArrayList<ObjectEffect> effects = new ArrayList<>();
     private int maxDurability = 100000;
     private double wearSpeed= 100;
     private int creationTime;
